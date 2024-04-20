c08ecdca6458bd66db647ac61d641b587507f8dc
Xudong Shi <iamshixudong@gmail.com>
2024-04-20 18:45:59 +0200

    Refactored created window: let it accept ref to vec, use lifetime

--- src/window.rs
+++ src/window.rs
@@ -3,54 +3,66 @@
 use std::ffi::{c_double, c_int, c_void};
 use std::mem::{ManuallyDrop, size_of};
 use std::ptr;
-use mpi_sys::{MPI_Aint, MPI_Win, RSMPI_COMM_WORLD, RSMPI_DOUBLE, RSMPI_INFO_NULL};
+use mpi_sys::{MPI_Aint, MPI_Win, RSMPI_COMM_WORLD, RSMPI_INFO_NULL};
 use crate::ffi;
+use crate::traits::{AsRaw, Equivalence};
 
-pub struct CreatedWindow {
-    pub window_vector: Vec<f64>,
-    pub window_ptr: MPI_Win
+pub struct CreatedWindow<'a, T> where T: Equivalence {
+    pub window_vec_ptr: &'a mut Vec<T>,
+    pub window_base_ptr: MPI_Win
 }
 
-impl CreatedWindow {
-    pub fn new(size: usize) -> Self {
+impl<'a, T> CreatedWindow<'a, T> where T: Equivalence {
+    pub fn new(size: usize, vec_ptr: &'a mut Vec<T>) -> Self {
         let mut win = Self {
-            window_vector: vec![0f64; size],
-            window_ptr: ptr::null_mut()
+            window_vec_ptr: vec_ptr,
+            window_base_ptr: ptr::null_mut()
         };
         unsafe {
             ffi::MPI_Win_create(
-                win.window_vector.as_mut_ptr() as *mut c_void,
-                (size * size_of::<c_double>()) as MPI_Aint,
+                win.window_vec_ptr.as_mut_ptr() as *mut c_void,
+                (size * size_of::<f64>()) as MPI_Aint,
                 size_of::<c_double>() as c_int,
                 RSMPI_INFO_NULL,
                 RSMPI_COMM_WORLD,
-                &mut win.window_ptr
+                &mut win.window_base_ptr
             );
         }
         return win;
     }
 }
+impl<'a, T> WindowOperations for CreatedWindow<'a, T> where T: Equivalence {
+    fn get_whole_vector(&mut self, target_rank: usize) {
+        get(self.window_vec_ptr, target_rank, self.window_base_ptr);
+    }
+    fn put_whole_vector(&mut self, target_rank: usize) {
+        put(self.window_vec_ptr, target_rank, self.window_base_ptr);
+    }
+    fn fence(&self) {
+        fence(self.window_base_ptr);
+    }
+}
 
-pub struct AllocatedWindow {
-    pub window_vector: ManuallyDrop<Vec<f64>>,
+pub struct AllocatedWindow<T> where T: Equivalence {
+    pub window_vector: ManuallyDrop<Vec<T>>,
     pub window_ptr: MPI_Win
 }
 
-impl AllocatedWindow {
+impl<T> AllocatedWindow<T> where T: Equivalence{
     pub fn new(size: usize) -> Self {
-        let mut window_base: *mut f64 = ptr::null_mut();
+        let mut window_base: *mut T = ptr::null_mut();
         let mut window_handle: MPI_Win = ptr::null_mut();
         unsafe {
             ffi::MPI_Win_allocate(
-                (size * size_of::<c_double>()) as MPI_Aint,
-                size_of::<c_double>() as c_int,
+                (size * size_of::<T>()) as MPI_Aint,
+                size_of::<T>() as c_int,
                 RSMPI_INFO_NULL,
                 RSMPI_COMM_WORLD,
                 &mut window_base as *mut *mut _ as *mut c_void,
                 &mut window_handle
             );
             let win = Self {
-                window_vector: ManuallyDrop::new(unsafe { Vec::from_raw_parts(window_base, size, size) }),
+                window_vector: ManuallyDrop::new(Vec::from_raw_parts(window_base, size, size)),
                 window_ptr: window_handle
             };
             return win;
@@ -64,7 +76,8 @@ pub trait WindowOperations {
     fn fence(&self);
 }
 
-impl WindowOperations for CreatedWindow {
+
+impl<T> WindowOperations for AllocatedWindow<T> where T: Equivalence {
     fn get_whole_vector(&mut self, target_rank: usize) {
         get(&mut self.window_vector, target_rank, self.window_ptr);
     }
@@ -76,42 +89,30 @@ impl WindowOperations for CreatedWindow {
     }
 }
 
-impl WindowOperations for AllocatedWindow {
-    fn get_whole_vector(&mut self, target_rank: usize) {
-        get(&mut self.window_vector, target_rank, self.window_ptr);
-    }
-    fn put_whole_vector(&mut self, target_rank: usize) {
-        put(&mut self.window_vector, target_rank, self.window_ptr);
-    }
-    fn fence(&self) {
-        fence(self.window_ptr);
-    }
-}
-
-fn get(vec: &mut Vec<f64>, target_rank: usize, window: MPI_Win) {
+fn get<T>(vec: &mut Vec<T>, target_rank: usize, window: MPI_Win) where T: Equivalence {
     unsafe {
         ffi::MPI_Get(
             vec.as_mut_ptr() as *mut c_void,
             vec.len() as c_int,
-            RSMPI_DOUBLE,
+            T::equivalent_datatype().as_raw(),
             target_rank as c_int,
             0,
             vec.len() as c_int,
-            RSMPI_DOUBLE,
+            T::equivalent_datatype().as_raw(),
             window
         );
     }
 }
-fn put(vec: &mut Vec<f64>, target_rank: usize, window: MPI_Win) {
+fn put<T>(vec: &mut Vec<T>, target_rank: usize, window: MPI_Win) where T: Equivalence {
     unsafe {
         ffi::MPI_Put(
             vec.as_mut_ptr() as *mut c_void,
             vec.len() as c_int,
-            RSMPI_DOUBLE,
+            T::equivalent_datatype().as_raw(),
             target_rank as c_int,
             0,
             vec.len() as c_int,
-            RSMPI_DOUBLE,
+            T::equivalent_datatype().as_raw(),
             window
         );
     }
