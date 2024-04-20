fa04ee9046e25ae7d0aac50f1e0fa2ed73926c2d
Xudong Shi <iamshixudong@gmail.com>
2024-04-20 02:57:52 +0200

    Introduced type from rsmpi library, but came across free(): invalid pointer at window.rs get method

--- src/window.rs
+++ src/window.rs
@@ -3,47 +3,32 @@
 use std::ffi::{c_double, c_int, c_void};
 use std::mem::{ManuallyDrop, size_of};
 use std::ptr;
-use mpi_sys::{MPI_Aint, MPI_Win, RSMPI_COMM_WORLD, RSMPI_DOUBLE, RSMPI_INFO_NULL};
+use mpi_sys::{MPI_Aint, MPI_Datatype, MPI_Win, RSMPI_COMM_WORLD, RSMPI_DOUBLE, RSMPI_INFO_NULL};
+use crate::datatype::{Buffer, Equivalence, SystemDatatype, UserDatatype};
 use crate::ffi;
+use crate::raw::AsRaw;
+use crate::traits::{BufferMut, UncommittedDatatype};
 
-pub struct CreatedWindow {
-    pub window_vector: Vec<f64>,
+pub struct CreatedWindow<T> where T: Equivalence {
+    pub window_vector: Vec<T>,
     pub window_ptr: MPI_Win
 }
 
-impl CreatedWindow {
-    pub fn new(size: usize) -> Self {
-        let mut win = Self {
-            window_vector: vec![0f64; size],
-            window_ptr: ptr::null_mut()
-        };
-        unsafe {
-            ffi::MPI_Win_create(
-                win.window_vector.as_mut_ptr() as *mut c_void,
-                (size * size_of::<c_double>()) as MPI_Aint,
-                size_of::<c_double>() as c_int,
-                RSMPI_INFO_NULL,
-                RSMPI_COMM_WORLD,
-                &mut win.window_ptr
-            );
-        }
-        return win;
-    }
-}
 
-pub struct AllocatedWindow {
-    pub window_vector: ManuallyDrop<Vec<f64>>,
+pub struct AllocatedWindow<T> where T: Equivalence {
+    pub window_vector: ManuallyDrop<Vec<T>>,
     pub window_ptr: MPI_Win
 }
 
-impl AllocatedWindow {
+impl<T> AllocatedWindow<T> where T: Equivalence {
     pub fn new(size: usize) -> Self {
-        let mut window_base: *mut f64 = ptr::null_mut();
+        println!("Now starting to allocate");
+        let mut window_base: *mut T = ptr::null_mut();
         let mut window_handle: MPI_Win = ptr::null_mut();
         unsafe {
             ffi::MPI_Win_allocate(
-                (size * size_of::<c_double>()) as MPI_Aint,
-                size_of::<c_double>() as c_int,
+                (size * size_of::<T>()) as MPI_Aint,
+                size_of::<T>() as c_int,
                 RSMPI_INFO_NULL,
                 RSMPI_COMM_WORLD,
                 &mut window_base as *mut *mut _ as *mut c_void,
@@ -53,6 +38,7 @@ impl AllocatedWindow {
                 window_vector: ManuallyDrop::new(unsafe { Vec::from_raw_parts(window_base, size, size) }),
                 window_ptr: window_handle
             };
+            println!("Allocated");
             return win;
         }
     }
@@ -64,54 +50,44 @@ pub trait WindowOperations {
     fn fence(&self);
 }
 
-impl WindowOperations for CreatedWindow {
+impl<T> WindowOperations for AllocatedWindow<T> where T: Equivalence {
     fn get_whole_vector(&mut self, target_rank: usize) {
-        get(&mut self.window_vector, target_rank, self.window_ptr);
+        println!("Before getting into unsafe");
+        unsafe { put(&mut ManuallyDrop::take(&mut self.window_vector), target_rank, self.window_ptr); }
     }
     fn put_whole_vector(&mut self, target_rank: usize) {
-        put(&mut self.window_vector, target_rank, self.window_ptr);
+        unsafe { put(&mut ManuallyDrop::take(&mut self.window_vector), target_rank, self.window_ptr); }
     }
     fn fence(&self) {
         fence(self.window_ptr);
     }
 }
 
-impl WindowOperations for AllocatedWindow {
-    fn get_whole_vector(&mut self, target_rank: usize) {
-        get(&mut self.window_vector, target_rank, self.window_ptr);
-    }
-    fn put_whole_vector(&mut self, target_rank: usize) {
-        put(&mut self.window_vector, target_rank, self.window_ptr);
-    }
-    fn fence(&self) {
-        fence(self.window_ptr);
-    }
-}
-
-fn get(vec: &mut Vec<f64>, target_rank: usize, window: MPI_Win) {
+fn get<Buf: ?Sized>(vec: &mut Buf, target_rank: usize, window: MPI_Win) where Buf: BufferMut {
+    println!("I'm inside of unsafe inside now");
     unsafe {
         ffi::MPI_Get(
-            vec.as_mut_ptr() as *mut c_void,
-            vec.len() as c_int,
-            RSMPI_DOUBLE,
+            vec.pointer_mut(),
+            vec.count(),
+            vec.as_datatype().as_raw(),
             target_rank as c_int,
             0,
-            vec.len() as c_int,
-            RSMPI_DOUBLE,
+            vec.count(),
+            vec.as_datatype().as_raw(),
             window
         );
     }
 }
-fn put(vec: &mut Vec<f64>, target_rank: usize, window: MPI_Win) {
+fn put<Buf: ?Sized>(vec: &mut Buf, target_rank: usize, window: MPI_Win) where Buf: BufferMut {
     unsafe {
         ffi::MPI_Put(
-            vec.as_mut_ptr() as *mut c_void,
-            vec.len() as c_int,
-            RSMPI_DOUBLE,
+            vec.pointer_mut(),
+            vec.count(),
+            vec.as_datatype().as_raw(),
             target_rank as c_int,
             0,
-            vec.len() as c_int,
-            RSMPI_DOUBLE,
+            vec.count(),
+            vec.as_datatype().as_raw(),
             window
         );
     }
@@ -120,5 +96,15 @@ fn put(vec: &mut Vec<f64>, target_rank: usize, window: MPI_Win) {
 fn fence(window: MPI_Win) {
     unsafe {
         ffi::MPI_Win_fence(0, window);
+    }
+}
+
+impl<T> Drop for AllocatedWindow<T> where T: Equivalence {
+    fn drop(&mut self) {
+        println!("Drop is called");
+        unsafe {
+            ffi::MPI_Win_free(&mut self.window_ptr);
+        }
+        println!("Window has been free");
     }
 }
