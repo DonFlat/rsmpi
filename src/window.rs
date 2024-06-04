b25a1b80d64e7cc571268e7014a70b893a38bf35
Xudong Shi <iamshixudong@gmail.com>
2024-06-04 15:14:37 +0200

    Put from vector method

--- src/window.rs
+++ src/window.rs
@@ -2,7 +2,7 @@
 
 use std::ffi::{c_int, c_void};
 use std::mem::{ManuallyDrop};
-use mpi_sys::{MPI_Aint, MPI_Put, MPI_Win, RSMPI_COMM_WORLD, RSMPI_INFO_NULL};
+use mpi_sys::{MPI_Aint, MPI_Win};
 use crate::ffi;
 use crate::traits::{AsRaw, Equivalence};
 
@@ -16,6 +16,21 @@ pub struct AllocatedWindow<T> where T: Equivalence {
     pub window_ptr: MPI_Win
 }
 
+pub trait WindowOperations <T> {
+    fn get_whole_vector(&mut self, target_rank: usize);
+    fn put(
+        &mut self,
+        origin_disp: usize,
+        origin_count: usize,
+        target_rank: usize,
+        target_disp: usize,
+        target_count: usize
+    );
+    fn put_whole_vector(&mut self, target_rank: usize);
+    fn put_from_vector(&mut self, origin: &mut Vec<T>, target_rank: usize);
+    fn fence(&self);
+}
+
 
 impl<'a, T> WindowOperations<T> for CreatedWindow<'a, T> where T: Equivalence {
     fn get_whole_vector(&mut self, target_rank: usize) {
@@ -24,7 +39,7 @@ impl<'a, T> WindowOperations<T> for CreatedWindow<'a, T> where T: Equivalence {
 
     fn put(&mut self, origin_disp: usize, origin_count: usize, target_rank: usize, target_disp: usize, target_count: usize) {
         unsafe {
-            MPI_Put(
+            ffi::MPI_Put(
                 self.window_vec_ptr.as_ptr().add(origin_disp) as *mut c_void,
                 origin_count as c_int,
                 T::equivalent_datatype().as_raw(),
@@ -37,28 +52,30 @@ impl<'a, T> WindowOperations<T> for CreatedWindow<'a, T> where T: Equivalence {
         }
     }
 
+
     fn put_whole_vector(&mut self, target_rank: usize) {
         put(self.window_vec_ptr, target_rank, self.window_base_ptr);
     }
+
+    fn put_from_vector(&mut self, origin: &mut Vec<T>, target_rank: usize) {
+        unsafe {
+            ffi::MPI_Put(
+                origin.as_mut_ptr() as *mut c_void,
+                origin.len() as c_int,
+                T::equivalent_datatype().as_raw(),
+                target_rank as c_int,
+                0,
+                origin.len() as c_int,
+                T::equivalent_datatype().as_raw(),
+                self.window_base_ptr
+            );
+        }
+    }
+
     fn fence(&self) {
         fence(self.window_base_ptr);
     }
 }
-
-pub trait WindowOperations <T> {
-    fn get_whole_vector(&mut self, target_rank: usize);
-    fn put(
-        &mut self,
-        origin_disp: usize,
-        origin_count: usize,
-        target_rank: usize,
-        target_disp: usize,
-        target_count: usize
-    );
-    fn put_whole_vector(&mut self, target_rank: usize);
-    fn fence(&self);
-}
-
 
 impl<T> WindowOperations<T> for AllocatedWindow<T> where T: Equivalence {
     fn get_whole_vector(&mut self, target_rank: usize) {
@@ -67,7 +84,7 @@ impl<T> WindowOperations<T> for AllocatedWindow<T> where T: Equivalence {
 
     fn put(&mut self, origin_disp: usize, origin_count: usize, target_rank: usize, target_disp: usize, target_count: usize) {
         unsafe {
-            MPI_Put(
+            ffi::MPI_Put(
                 self.window_vector.as_ptr().add(origin_disp) as *mut c_void,
                 origin_count as c_int,
                 T::equivalent_datatype().as_raw(),
@@ -82,6 +99,21 @@ impl<T> WindowOperations<T> for AllocatedWindow<T> where T: Equivalence {
 
     fn put_whole_vector(&mut self, target_rank: usize) {
         put(&mut self.window_vector, target_rank, self.window_ptr);
+    }
+
+    fn put_from_vector(&mut self, origin: &mut Vec<T>, target_rank: usize) {
+        unsafe {
+            ffi::MPI_Put(
+                origin.as_mut_ptr() as *mut c_void,
+                origin.len() as c_int,
+                T::equivalent_datatype().as_raw(),
+                target_rank as c_int,
+                0,
+                origin.len() as c_int,
+                T::equivalent_datatype().as_raw(),
+                self.window_ptr
+            );
+        }
     }
     fn fence(&self) {
         fence(self.window_ptr);
